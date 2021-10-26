df4ff8083faa67c9b299e4095d7d012517c349b7
ChristopherZito <87862547+ChristopherZito@users.noreply.github.com>
2021-10-26 15:07:55 +0200

    consegna dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,3 +1,21 @@
 // Generare un numero random da 1 a 6,
 // sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const win = document.getElementById("winner");
+const numUs = document.getElementById("num-us");
+const numPc = document.getElementById("numpc");
+
+let diceUs = Math.floor(Math.random() * 6) +1;
+let dicePc = Math.floor(Math.random() * 6) +1;
+
+numUs.innerHTML = "il valore dell'utente è: " + diceUs;
+numPc.innerHTML = "il valore del computer è: " + dicePc;
+
+if(dicePc < diceUs){
+    win.innerHTML = "il vincitore è l'utente";
+}else if(dicePc === diceUs){
+    win.innerHTML = "i dadi hanno lo stesso valore";
+}else {
+    win.innerHTML = "il vincitore è il PC";
+}
